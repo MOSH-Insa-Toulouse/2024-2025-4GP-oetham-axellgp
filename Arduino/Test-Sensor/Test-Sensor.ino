486317322ec9fd3c08dc54b6919050dfe94fd161
oetham <matheohahn@gmail.com>
2025-03-31 15:47:30 +0200

    Fonctionalite du menu

--- Arduino/Test-Sensor/Test-Sensor.ino
+++ Arduino/Test-Sensor/Test-Sensor.ino
@@ -4,36 +4,39 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <RunningAverage.h>
-RunningAverage myRA(20);
-#define WAIT_DELAY 5000
+#include <stdlib.h>
+
+#define WAIT_DELAY               5000
 #define nombreDePixelsEnLargeur 128         // Taille de l'écran OLED, en pixel, au niveau de sa largeur
 #define nombreDePixelsEnHauteur 64          // Taille de l'écran OLED, en pixel, au niveau de sa hauteur
 #define brocheResetOLED         -1          // Reset de l'OLED partagé avec l'Arduino (d'où la valeur à -1, et non un numéro de pin)
 #define adresseI2CecranOLED     0x3C   
-#define MCP_NOP 0b00000000
-#define MCP_WRITE 0b00010001
-#define MCP_SHTDWN 0b00100001
+#define MCP_NOP                 0b00000000
+#define MCP_WRITE               0b00010001
+#define MCP_SHTDWN              0b00100001
+
+#define encoder0PinA            2  //CLK Output A Do not use other pin for clock as we are using interrupt
+#define encoder0PinB            4  //DT Output B
+#define RX_PIN                  7
+#define TX_PIN                  8
+#define Switch                  6 // Switch connection if available
+#define flexPin                 A1
+#define amplificateur           A0
+#define servomoteur             9
+#define ssMCPin                 10
+
+#define etat                    5
+
 // Définir les broches pour SoftwareSerial
 Adafruit_SSD1306 ecranOLED(nombreDePixelsEnLargeur, nombreDePixelsEnHauteur, &Wire, brocheResetOLED);
-Servo myservo;
-
-#define encoder0PinA  2  //CLK Output A Do not use other pin for clock as we are using interrupt
-#define encoder0PinB  4  //DT Output B
-#define Switch 6 // Switch connection if available
-const int RX_PIN = 7;
-const int TX_PIN = 8;
-byte position = 0;
+Servo myServo;
+RunningAverage myRA(20);
 SoftwareSerial mySerial(RX_PIN, TX_PIN);
+
+byte position = 0;
 volatile int encoder0Pos = 0;
-
-int posmoteur=0;
-
-int flexpin=A1;
-int amplicapteur =A0;
-int servomoteur = 9 ;
-int etat=5;
-const int ssMCPin = 10;
-int oldPOSencodeur=0;
+int posmoteur = 0, oldPOSencodeur = 0;
+unsigned long previousMillis = 0;
 
 void setup() {
   myRA.clear();
@@ -53,10 +56,9 @@ void setup() {
   digitalWrite(encoder0PinB, HIGH);       // turn on pullup resistor
 
   attachInterrupt(0, doEncoder, RISING);
-  pinMode(flexpin,INPUT); 
+  pinMode(flexPin,INPUT); 
   
-  myservo.attach(9);
-  // put your setup code here, to run once:
+  myServo.attach(9);
   Serial.begin(9600);
   mySerial.begin(9600);
   Serial.println(F("[MCP Digital Pot Test]"));
@@ -65,10 +67,10 @@ void setup() {
 void loop() {
   switch(etat){
     case 1:
-      Serial.println(analogRead(flexpin));
+      Serial.println(analogRead(flexPin));
       break;
     case 2 :
-      myservo.write(posmoteur);
+      myServo.write(posmoteur);
       delay(100);
       posmoteur++;
       break;
@@ -84,57 +86,98 @@ void loop() {
       }
 
   // Vérifier si des données sont disponibles sur Serial
-        if (Serial.available()) {
-          // Lire les données de Serial
-          char data = Serial.read();
-          // Envoyer les données à mySerial
-          mySerial.print(data);
-        }
-        break;
-      case 5 : //oled
-        
-        ecranOLED.clearDisplay();                                   // Effaçage de l'intégralité du buffer
-        ecranOLED.setTextSize(3);                   // Taille des caractères (1:1, puis 2:1, puis 3:1)
-        ecranOLED.setCursor(0, 0);                                  // Déplacement du curseur en position (0,0), c'est à dire dans l'angle supérieur gauche
-
-        
-        ecranOLED.setTextColor(SSD1306_WHITE,SSD1306_BLACK);   // Couleur du texte, et couleur du fond
-                            // Affichage du texte en "blanc" (avec la couleur principale, en fait, car l'écran monochrome peut être coloré)
-        ecranOLED.println(" Hello ");
-        ecranOLED.setTextSize(2);
-        ecranOLED.println(" Test Oled");
-        ecranOLED.print("RotE =");
-        ecranOLED.print(encoder0Pos,DEC);
-            
-        ecranOLED.display();                            // Transfert le buffer à l'écran
-        delay(50);
-
-      case 6:
+      if (Serial.available()) {
+        // Lire les données de Serial
+        char data = Serial.read();
+        // Envoyer les données à mySerial
+        mySerial.print(data);
+      }
+      break;
+    case 5 : //oled
       
+      menuChoice();
+
+    case 6:
+    
       if (oldPOSencodeur != encoder0Pos) {
         SPIWrite(MCP_WRITE, abs(encoder0Pos)%256, ssMCPin);
         delay(100);
-        oldPOSencodeur= encoder0Pos;}
-        myRA.addValue(analogRead(flexpin));
+        oldPOSencodeur= encoder0Pos;
+      }
+      myRA.addValue(analogRead(flexPin));
       
       Serial.println(myRA.getAverage());
-
-
-      
+    }
   }
-
-  
-  // put your main code here, to run repeatedly:
-
 }
+
+/*    
+*     Menu Choice :
+*     1 : Graphite Sensor
+*     2 : Flex Sensor
+*     3 : Servo Motor
+*/
+
+void menuChoice() {
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 500) {
+    previousMillis = currentMillis;
+    ecranOLED.clearDisplay();                                   // Effaçage de l'intégralité du buffer
+    ecranOLED.setTextSize(2);                   // Taille des caractères (1:1, puis 2:1, puis 3:1)
+    ecranOLED.setCursor(0, 0);
+    ecranOLED.setTextColor(SSD1306_WHITE,SSD1306_BLACK);
+    ecranOLED.println("---MENU---");
+    ecranOLED.setTextSize(1);
+
+    switch (abs(encoder0Pos)) {
+      case 0 :                                 // Déplacement du curseur en position (0,0), c'est à dire dans l'angle supérieur gauche
+        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+        ecranOLED.println("1. Graphite Sensor");
+        ecranOLED.setTextColor(SSD1306_WHITE,SSD1306_BLACK);   // Couleur du texte, et couleur du fond
+                            // Affichage du texte en "blanc" (avec la couleur principale, en fait, car l'écran monochrome peut être coloré)
+        ecranOLED.println("2. Flex Sensor");
+        ecranOLED.println("3. Servo Motor");
+        ecranOLED.display(); 
+        break;
+      
+      case 1 :
+          
+      // ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE); 
+        ecranOLED.println("1. Graphite Sensor");         
+        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE);         
+        ecranOLED.println("2. Flex Sensor");
+        ecranOLED.setTextColor(SSD1306_WHITE,SSD1306_BLACK);
+        ecranOLED.println("3. Servo Motor");
+        ecranOLED.display(); 
+        break;
+
+      case 2 :
+        ecranOLED.println("1. Graphite Sensor");         
+        ecranOLED.println("2. Flex Sensor");
+        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE); 
+        ecranOLED.println("3. Servo Motor");
+        ecranOLED.display(); 
+        break;
+    }
+  }
+  delay(50);
+}
+
+
 void doEncoder() {
   if (digitalRead(encoder0PinA)==HIGH && digitalRead(encoder0PinB)==HIGH) {
     encoder0Pos++;
-  } else if (digitalRead(encoder0PinA)==HIGH && digitalRead(encoder0PinB)==LOW) {
-    encoder0Pos--;
+  } 
+  else if (digitalRead(encoder0PinA)==HIGH && digitalRead(encoder0PinB)==LOW) {
+    encoder0Pos--;   
   }
-  // Serial.println (encoder0Pos, DEC);  //Angle = (360 / Encoder_Resolution) * encoder0Pos
+  encoder0Pos %= 3;
+
+  //Serial.println (encoder0PinA, DEC);  //Angle = (360 / Encoder_Resolution) * encoder0Pos
 }
+
 
 void SPIWrite(uint8_t cmd, uint8_t data, uint8_t ssPin) // SPI write the command and data to the MCP IC connected to the ssPin
 {
